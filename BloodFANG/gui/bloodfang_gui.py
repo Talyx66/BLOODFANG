4a9059a2eb11a823cb581d27dab3467f4d06459c
Talyx <Tylercobey@hotmail.com>
2025-07-01 14:45:33 -0400

    Update bloodfang_gui.py

--- BloodFANG/gui/bloodfang_gui.py
+++ BloodFANG/gui/bloodfang_gui.py
@@ -83,7 +83,7 @@ class BloodFangGUI(QMainWindow):
         self.central_widget.setGraphicsEffect(shadow)
 
         # Transparent overlay so GIF shows through
-        self.central_widget.setStyleSheet("background-color: rgba(20, 20, 20, 100);")
+        self.central_widget.setStyleSheet("background-color: rgba(20, 20, 20, 70);")
 
         layout = QVBoxLayout()
 
